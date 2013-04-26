9c7056f2c7e1ab597c8fa98e57012814460a11b0
vishal sodani <vishal_sodani@yahoo.com>
2013-04-26 10:31:41 +0530

    refactored common code

--- track_activity/tests.py
+++ track_activity/tests.py
@@ -9,9 +9,9 @@ from django.test import TestCase
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-from django.contrib.auth.models import User
 import time
 from django.test import LiveServerTestCase
+from test_helper import create_superuser,login_admin_user
 
 
 class authorised_user_should_be_able_to_add_an_activity(LiveServerTestCase):
@@ -19,19 +19,15 @@ class authorised_user_should_be_able_to_add_an_activity(LiveServerTestCase):
         """
         Create an admin user, login, browse to add_activity,add an activity and confirm activity added.
         """
-        User.objects.create_superuser(username='admin',
-                                      password='test',
-                                      email='test@test.com')
+        create_superuser()
 
         browser = webdriver.Firefox()
-        browser.get("http://127.0.0.1:8081/admin")
-        browser.find_element_by_name("username").send_keys("admin")
-        browser.find_element_by_name("password").send_keys('test')
-        browser.find_element_by_xpath('//input[@value="Log in"]').click()
+        login_admin_user(browser)
         browser.get("http://127.0.0.1:8081/my_activity/add_activity")
         browser.find_element_by_name("name").send_keys("  Walking ")
         browser.find_element_by_xpath('//input[@value="Save"]').click()
         elem = browser.find_element_by_xpath('//td[1]')
+        
         self.assertEqual(elem.text, 'Walking')
         browser.quit()
 
@@ -41,20 +37,15 @@ class authorised_user_should_not_be_able_to_add_an_empty_activity(LiveServerTest
         """
         Create an admin user, login, browse to add_activity,add an empty activity and confirm error message.
         """
-        User.objects.create_superuser(username='admin',
-                                      password='test',
-                                      email='test@test.com')
+        create_superuser()
 
         browser = webdriver.Firefox()
-        browser.get("http://127.0.0.1:8081/admin")
-        browser.find_element_by_name("username").send_keys("admin")
-        browser.find_element_by_name("password").send_keys('test')
-        browser.find_element_by_xpath('//input[@value="Log in"]').click()
+        login_admin_user(browser)
         browser.get("http://127.0.0.1:8081/my_activity/add_activity")
         browser.find_element_by_name("name").send_keys("")
         browser.find_element_by_xpath('//input[@value="Save"]').click()
         elem = browser.find_element_by_xpath('//ul[@class="errorlist"]')
-        print elem.text
+        
         self.assertEqual(elem.text, 'This field is required.')
         browser.quit()
 
