2bbe75f1bbae27c940da19583de11fda50437a40
vishal sodani <vishal_sodani@yahoo.com>
2013-04-28 18:38:46 +0530

    demonstrate use of Client to test a view

--- track_activity/tests.py
+++ track_activity/tests.py
@@ -6,7 +6,6 @@ import time
 from django.test import LiveServerTestCase
 from test_helper import create_superuser,login_admin_user,browse_to_add_activity_and_enter_name
 from django.utils import unittest
-from django.test.client import RequestFactory
 from track_activity.views import save_activity
 from django.test.client import Client
 
@@ -45,9 +44,6 @@ class authorised_user_should_not_be_able_to_add_an_empty_activity(LiveServerTest
 class unauthorised_user_should_not_be_able_toaccess_addactivity(unittest.TestCase):
 
     def test(self):
-
-        factory = RequestFactory()
-        request = factory.get('/my_activity/add_acivity')
         c = Client()
         response = c.get('/my_activity/add_acivity')
         self.assertEqual(response.status_code, 404) #currently no login feature so not exists       
