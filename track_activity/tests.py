fa9022168729e5dc18d61676fb579ba33e885dac
vishal sodani <vishal_sodani@yahoo.com>
2013-04-28 18:36:40 +0530

    demonstrate use of Client to test a view

--- track_activity/tests.py
+++ track_activity/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -12,6 +5,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
 from test_helper import create_superuser,login_admin_user,browse_to_add_activity_and_enter_name
+from django.utils import unittest
+from django.test.client import RequestFactory
+from track_activity.views import save_activity
+from django.test.client import Client
 
 
 class authorised_user_should_be_able_to_add_an_activity(LiveServerTestCase):
@@ -45,5 +42,13 @@ class authorised_user_should_not_be_able_to_add_an_empty_activity(LiveServerTest
         self.assertEqual(elem.text, 'This field is required.')
         browser.quit()
 
-        
+class unauthorised_user_should_not_be_able_toaccess_addactivity(unittest.TestCase):
+
+    def test(self):
+
+        factory = RequestFactory()
+        request = factory.get('/my_activity/add_acivity')
+        c = Client()
+        response = c.get('/my_activity/add_acivity')
+        self.assertEqual(response.status_code, 404) #currently no login feature so not exists       
         
