da4c7a50dd25a37de6b286481145caa9c60b7c14
vishal sodani <vishal_sodani@yahoo.com>
2013-04-28 21:28:14 +0530

    removed imports, was trying for making packages and run them separately

--- track_activity/selenium/__init__.py
+++
@@ -1,1 +0,0 @@
-from track_activity.selenium.tests import *
--- track_activity/unittests/__init__.py
+++
@@ -1,6 +0,0 @@
-from track_activity.unittests.tests import *
-
-###starts the test suite  
-##__test__= {  
-##           'unauthorised_user_should_not_be_able_toaccess_addactivity': tests
-##           }  
