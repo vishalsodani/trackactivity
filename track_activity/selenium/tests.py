896c355c805dfa2d81db793ccf313f27a34603b6
vishal sodani <vishal_sodani@yahoo.com>
2013-05-03 16:18:19 +0530

    using setup and teardown for selenium tests

--- track_activity/selenium/tests.py
+++ track_activity/selenium/tests.py
@@ -9,19 +9,26 @@ from test_helper import create_superuser,login_admin_user,browse_to_add_activity
 
 
 class authorised_user_should_be_able_to_add_an_activity(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+        
     def test(self):
         """
         Create an admin user, login, browse to add_activity,add an activity and confirm activity added.
         """
         create_superuser()
 
-        browser = webdriver.Firefox()
-        login_admin_user(browser)
-        browse_to_add_activity_and_enter_name(browser,'Walking')
-        elem = browser.find_element_by_xpath('//td[1]')
+        
+        login_admin_user(self.browser)
+        browse_to_add_activity_and_enter_name(self.browser,'Walking')
+        elem = self.browser.find_element_by_xpath('//td[1]')
         
         self.assertEqual(elem.text, 'Walking')
-        browser.quit()
+
 
 
 class authorised_user_should_not_be_able_to_add_an_empty_activity(LiveServerTestCase):
